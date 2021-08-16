39dda6b38e7d90971986a2f0359704702c1de050
Brahmaiah99 <88978479+Brahmaiah99@users.noreply.github.com>
2021-08-16 20:43:23 +0530

    switch case .c
    this file is print the switch case of choice

---
+++ switch-case.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int x=2;
+  switch (x)
+  {
+      case1:printf("choice is 1");
+       break;
+      case2:printf("choice is 2");
+       break;
+      case3:printf("choice is 3");
+      break;
+    default:printf("choice other than 1,2 nad 3");
+  }
+  return 0;
+}
